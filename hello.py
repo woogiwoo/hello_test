2a787df7ec727b552f08eb9cefa02c393783c134
woogiwoo <woogiwoo@gmail.com>
2024-05-24 00:40:50 +0900

    first python code

---
+++ hello.py
@@ -0,0 +1,10 @@
+def factorial(n) :
+    if n == 0 :
+	return :
+    else :
+	return n  * factorial(n-1)
+
+number = 5
+result = factorial(number)
+print(f"The factorial of {number} is {result}.")
+
